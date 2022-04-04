fb4e347cb3f248725aa32af2fdefb97bb653ee08
Johan Fylling <johan.dev@fylling.se>
2022-04-03 20:53:47 -0400

    Renaming '*-data' vars to '*-info'
    Signed-off-by: Johan Fylling <johan.dev@fylling.se>

--- src/opa4j/parser.clj
+++ src/opa4j/parser.clj
@@ -7,34 +7,34 @@
   ([msg] (println msg))
   ([msg & args] (println msg args)))
 
-(defn make-AssignVarStmt [stmt-data static]
+(defn make-AssignVarStmt [stmt-info static]
   "TODO"
   (log "making AssignVarStmt stmt")
   (fn [vars] (log "executing AssignVarStmt statement")))
 
-(defn make-BlockStmt [stmt-data static]
+(defn make-BlockStmt [stmt-info static]
   (log "making BlockStmt stmt")
-  (let [blocks (make-blocks (get (get stmt-data "stmt") "blocks") static)]
+  (let [blocks (make-blocks (get (get stmt-info "stmt") "blocks") static)]
     (fn [vars]
       (log "executing BlockStmt statement")
       (blocks vars))))
 
-(defn make-BreakStmt [stmt-data static]
+(defn make-BreakStmt [stmt-info static]
   "TODO"
   (log "making BreakStmt stmt")
   (fn [vars] (log "executing BreakStmt statement")))
 
-(defn make-CallStmt [stmt-data static]
+(defn make-CallStmt [stmt-info static]
   "TODO"
   (log "making CallStmt stmt")
   (fn [vars] (log "executing CallStmt statement")))
 
-(defn make-DotStmt [stmt-data static]
+(defn make-DotStmt [stmt-info static]
   "TODO"
   (log "making DotStmt stmt")
   (fn [vars] (log "executing DotStmt statement")))
 
-(defn make-MakeObjectStmt [stmt-data static]
+(defn make-MakeObjectStmt [stmt-info static]
   "TODO"
   (log "making MakeObjectStmt stmt")
   (fn [vars]
@@ -42,66 +42,66 @@
     ;(throw (Exception. "AssignVarStmt statement not implemented"))
     ))
 
-(defn make-ObjectInsertStmt [stmt-data static]
+(defn make-ObjectInsertStmt [stmt-info static]
   "TODO"
   (log "making ObjectInsertStmt stmt")
   (fn [vars] (log "executing ObjectInsertStmt statement")))
 
-(defn make-ObjectMergeStmt [stmt-data static]
+(defn make-ObjectMergeStmt [stmt-info static]
   "TODO"
   (log "making ObjectMergeStmt stmt")
   (fn [vars] (log "executing ObjectMergeStmt statement")))
 
-(defn make-ResultSetAddStmt [stmt-data static]
+(defn make-ResultSetAddStmt [stmt-info static]
   "TODO"
   (log "making ResultSetAddStmt stmt")
   (fn [vars] (log "executing ResultSetAddStmt statement")))
 
-(defn make-stmt [stmt-data static]
-  (log "making stmt" stmt-data)
-  (let [type (get stmt-data "type")]
+(defn make-stmt [stmt-info static]
+  (log "making stmt" stmt-info)
+  (let [type (get stmt-info "type")]
     (case type
-      "AssignVarStmt" (make-AssignVarStmt stmt-data static)
-      "BlockStmt" (make-BlockStmt stmt-data static)
-      "BreakStmt" (make-BreakStmt stmt-data static)
-      "CallStmt" (make-CallStmt stmt-data static)
-      "DotStmt" (make-DotStmt stmt-data static)
-      "MakeObjectStmt" (make-MakeObjectStmt stmt-data static)
-      "ObjectInsertStmt" (make-ObjectInsertStmt stmt-data static)
-      "ObjectMergeStmt" (make-ObjectMergeStmt stmt-data static)
-      "ResultSetAddStmt" (make-ResultSetAddStmt stmt-data static)
+      "AssignVarStmt" (make-AssignVarStmt stmt-info static)
+      "BlockStmt" (make-BlockStmt stmt-info static)
+      "BreakStmt" (make-BreakStmt stmt-info static)
+      "CallStmt" (make-CallStmt stmt-info static)
+      "DotStmt" (make-DotStmt stmt-info static)
+      "MakeObjectStmt" (make-MakeObjectStmt stmt-info static)
+      "ObjectInsertStmt" (make-ObjectInsertStmt stmt-info static)
+      "ObjectMergeStmt" (make-ObjectMergeStmt stmt-info static)
+      "ResultSetAddStmt" (make-ResultSetAddStmt stmt-info static)
       (throw (Exception. (format "%s statement type not implemented" type))))))
 
-(defn make-stmts [stmts-data static]
+(defn make-stmts [stmts-info static]
   (log "making stmts")
-  (let [stmts (doall (for [stmt-data stmts-data]
-                       (make-stmt stmt-data static)))]
+  (let [stmts (doall (for [stmt-info stmts-info]
+                       (make-stmt stmt-info static)))]
     (fn [vars]
       (log "executing statements")
       (doseq [stmt stmts] (stmt vars)))))
 
-(defn make-block [block-data static]
+(defn make-block [block-info static]
   (log "making block")
-  (let [stmts-data (get block-data "stmts")]
-    (let [stmts (make-stmts stmts-data static)]
+  (let [stmts-info (get block-info "stmts")]
+    (let [stmts (make-stmts stmts-info static)]
       (fn [vars]
         (log "executing block")
         (stmts vars)))))
 
-(defn make-blocks [blocks-data static]
+(defn make-blocks [blocks-info static]
   (log "making blocks")
-  (let [blocks (doall (for [block-data blocks-data]
-                        (make-block block-data static)))]
+  (let [blocks (doall (for [block-info blocks-info]
+                        (make-block block-info static)))]
     (fn [vars]
       (log "executing blocks")
       (doseq [block blocks] (block vars)))))
 
-(defn make-plan [plan-data static]
-  (let [name (get plan-data "name") blocks-data (get plan-data "blocks")]
+(defn make-plan [plan-info static]
+  (let [name (get plan-info "name") blocks-info (get plan-info "blocks")]
     (log "making plan" name)
     (log "static:" static)
-    (log "plan:" plan-data)
-    (let [blocks (make-blocks blocks-data static)]
+    (log "plan:" plan-info)
+    (let [blocks (make-blocks blocks-info static)]
       (fn [vars]
         (log "executing plan" name)
         (blocks vars)))))
