3ffa64e4b083ef4619abdfd72897bb4574030894
Johan Fylling <johan.dev@fylling.se>
2022-04-04 16:12:46 -0400

    Updating logging functions
    Signed-off-by: Johan Fylling <johan.dev@fylling.se>

--- src/opa4j/parser.clj
+++ src/opa4j/parser.clj
@@ -4,61 +4,76 @@
 (declare make-blocks)
 
 (defn log
-  ([msg] (println msg))
-  ([msg & args] (println msg args)))
+  ([level msg]
+   (let [now (java.time.Instant/now)]
+     (println (format "%s: %s %s" now level msg))))
+  ([level msg & args]
+   (log level (apply format msg args))))
+
+(defn log-info
+  ([msg] (log "INFO" msg))
+  ([msg & args] (apply log "INFO" msg args)))
+
+(defn log-debug
+  ([msg] (log "DEBUG" msg))
+  ([msg & args] (apply log "DEBUG" msg args)))
+
+(defn log-trace
+  ([msg] (log "TRACE" msg))
+  ([msg & args] (apply log "TRACE" msg args)))
 
 (defn make-AssignVarStmt [stmt-info static]
   "TODO"
-  (log "making AssignVarStmt stmt")
-  (fn [vars] (log "executing AssignVarStmt statement")))
+  (log-debug "making AssignVarStmt stmt")
+  (fn [vars] (log-debug "executing AssignVarStmt statement")))
 
 (defn make-BlockStmt [stmt-info static]
-  (log "making BlockStmt stmt")
+  (log-debug "making BlockStmt stmt")
   (let [blocks (make-blocks (get (get stmt-info "stmt") "blocks") static)]
     (fn [vars]
-      (log "executing BlockStmt statement")
+      (log-debug "executing BlockStmt statement")
       (blocks vars))))
 
 (defn make-BreakStmt [stmt-info static]
   "TODO"
-  (log "making BreakStmt stmt")
-  (fn [vars] (log "executing BreakStmt statement")))
+  (log-debug "making BreakStmt stmt")
+  (fn [vars] (log-debug "executing BreakStmt statement")))
 
 (defn make-CallStmt [stmt-info static]
   "TODO"
-  (log "making CallStmt stmt")
-  (fn [vars] (log "executing CallStmt statement")))
+  (log-debug "making CallStmt stmt")
+  (fn [vars] (log-debug "executing CallStmt statement")))
 
 (defn make-DotStmt [stmt-info static]
   "TODO"
-  (log "making DotStmt stmt")
-  (fn [vars] (log "executing DotStmt statement")))
+  (log-debug "making DotStmt stmt")
+  (fn [vars] (log-debug "executing DotStmt statement")))
 
 (defn make-MakeObjectStmt [stmt-info static]
   "TODO"
-  (log "making MakeObjectStmt stmt")
+  (log-debug "making MakeObjectStmt stmt")
   (fn [vars]
-    (log "executing MakeObjectStmt statement")
+    (log-debug "executing MakeObjectStmt statement")
     ;(throw (Exception. "AssignVarStmt statement not implemented"))
     ))
 
 (defn make-ObjectInsertStmt [stmt-info static]
   "TODO"
-  (log "making ObjectInsertStmt stmt")
-  (fn [vars] (log "executing ObjectInsertStmt statement")))
+  (log-debug "making ObjectInsertStmt stmt")
+  (fn [vars] (log-debug "executing ObjectInsertStmt statement")))
 
 (defn make-ObjectMergeStmt [stmt-info static]
   "TODO"
-  (log "making ObjectMergeStmt stmt")
-  (fn [vars] (log "executing ObjectMergeStmt statement")))
+  (log-debug "making ObjectMergeStmt stmt")
+  (fn [vars] (log-debug "executing ObjectMergeStmt statement")))
 
 (defn make-ResultSetAddStmt [stmt-info static]
   "TODO"
-  (log "making ResultSetAddStmt stmt")
-  (fn [vars] (log "executing ResultSetAddStmt statement")))
+  (log-debug "making ResultSetAddStmt stmt")
+  (fn [vars] (log-debug "executing ResultSetAddStmt statement")))
 
 (defn make-stmt [stmt-info static]
-  (log "making stmt" stmt-info)
+  (log-debug "making stmt" stmt-info)
   (let [type (get stmt-info "type")]
     (case type
       "AssignVarStmt" (make-AssignVarStmt stmt-info static)
@@ -73,37 +88,37 @@
       (throw (Exception. (format "%s statement type not implemented" type))))))
 
 (defn make-stmts [stmts-info static]
-  (log "making stmts")
+  (log-debug "making stmts")
   (let [stmts (doall (for [stmt-info stmts-info]
                        (make-stmt stmt-info static)))]
     (fn [vars]
-      (log "executing statements")
+      (log-debug "executing statements")
       (doseq [stmt stmts] (stmt vars)))))
 
 (defn make-block [block-info static]
-  (log "making block")
+  (log-debug "making block")
   (let [stmts-info (get block-info "stmts")]
     (let [stmts (make-stmts stmts-info static)]
       (fn [vars]
-        (log "executing block")
+        (log-debug "executing block")
         (stmts vars)))))
 
 (defn make-blocks [blocks-info static]
-  (log "making blocks")
+  (log-debug "making blocks")
   (let [blocks (doall (for [block-info blocks-info]
                         (make-block block-info static)))]
     (fn [vars]
-      (log "executing blocks")
+      (log-debug "executing blocks")
       (doseq [block blocks] (block vars)))))
 
 (defn make-plan [plan-info static]
   (let [name (get plan-info "name") blocks-info (get plan-info "blocks")]
-    (log "making plan" name)
-    (log "static:" static)
-    (log "plan:" plan-info)
+    (log-debug "making plan '%s'" name)
+    (log-trace "static: %s" static)
+    (log-trace "plan: %s" plan-info)
     (let [blocks (make-blocks blocks-info static)]
       (fn [vars]
-        (log "executing plan" name)
+        (log-debug "executing plan '%s'" name)
         (blocks vars)))))
 
 (defn parse
@@ -116,5 +131,5 @@
   "Reads and parses the incoming file"
   ([] (println "No file provided!"))
   ([file]
-   (log "Parsing file" file)
+   (log-debug "Parsing file '%s'" file)
    (parse (slurp file))))
