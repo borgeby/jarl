339eda1f3008bece9679e1bbb7252f94cdf89ca6
Johan Fylling <johan.dev@fylling.se>
2022-04-15 13:53:21 +0200

    Setting description and license in project.clj
    Signed-off-by: Johan Fylling <johan.dev@fylling.se>

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject opa4j "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "An OPA evaluator for the JVM"
+  :url "https://github.com/johanfylling/opa4j"
+  :license {:name "Apache License Version 2.0"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]]
   :plugins [[lein-ancient "1.0.0-RC3"]
