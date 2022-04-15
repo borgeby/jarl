bc9fb9a8ccc8b0247d2299450c6dd7c5194a9c8c
Johan Fylling <johan.dev@fylling.se>
2022-04-15 16:31:04 +0200

    Attempting to build custom java API
    and running tests on it.
    
    Signed-off-by: Johan Fylling <johan.dev@fylling.se>

--- src/main/clojure/opa4j/parser.clj
+++ src/main/clojure/opa4j/parser.clj
@@ -1,18 +1,23 @@
-;Copyright 2022 Johan Fylling, Anders Eknert
 ;
-;Licensed under the Apache License, Version 2.0 (the "License");
-;you may not use this file except in compliance with the License.
-;You may obtain a copy of the License at
+; Copyright 2022 Johan Fylling, Anders Eknert
 ;
-;       http://www.apache.org/licenses/LICENSE-2.0
+; Licensed under the Apache License, Version 2.0 (the "License")
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
 ;
-;Unless required by applicable law or agreed to in writing, software
-;distributed under the License is distributed on an "AS IS" BASIS,
-;WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-;See the License for the specific language governing permissions and
-;limitations under the License.
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+;
 
 (ns opa4j.parser
+  (:gen-class
+    ;:name "se.fylling.opa4j.Parser"
+    )
   (:require [clojure.data.json :as json])
   (:require [clojure.edn :as edn])
   (:require [clojure.string :as str])
