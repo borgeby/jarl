fb71c450267be943ec63b41e324829871c074bad
Anders Eknert <anders@eknert.com>
2022-04-28 15:42:15 +0200

    Refactor built-in type checking (#12)
    Signed-off-by: Anders Eknert <anders@eknert.com>

--- src/main/clojure/jarl/builtins/array.clj
+++ src/main/clojure/jarl/builtins/array.clj
@@ -1,39 +1,25 @@
 (ns jarl.builtins.array
-  (:import (se.fylling.jarl BuiltinException)
-           (clojure.lang PersistentVector)))
+  (:require [jarl.builtins.utils :refer [check-args]]))
 
-; TODO: Move somewhere else for reuse
-(defn java->rego [value]
-  (condp instance? value
-    String "string"
-    Boolean "boolean"
-    Double "floating-point number"
-    Float "floating-point number"
-    Integer "number"
-    Long "number"
-    Number "number"
-    PersistentVector "array"
-    (str "unknown type: " (type value) " from value: " value)))
-
-(defn builtin-concat [a b]
-  (when-not (vector? a)
-    (throw (BuiltinException. (format "array.concat: operand 1 must be array but got %s", (java->rego a)))))
-  (when-not (vector? b)
-    (throw (BuiltinException. (format "array.concat: operand 2 must be array but got %s", (java->rego b)))))
+(defn builtin-concat
+  "Implementation of array.concat built-in"
+  {:builtin "array.concat" :args-types ["array" "array"]}
+  [a b]
+  (check-args (meta #'builtin-concat) a b)
   (concat a b))
 
-(defn builtin-reverse [arr]
-  (when-not (vector? arr)
-    (throw (BuiltinException. (format "array.reverse: operand 1 must be array but got %s", (java->rego arr)))))
+(defn builtin-reverse
+  "Implementation of array.reverse built-in"
+  {:builtin "array.reverse" :args-types ["array"]}
+  [arr]
+  (check-args (meta #'builtin-reverse) arr)
   (reverse arr))
 
-(defn builtin-slice [arr start stop]
-  (when-not (vector? arr)
-    (throw (BuiltinException. (format "array.slice: operand 1 must be array but got %s", (java->rego arr)))))
-  (when-not (int? start)
-    (throw (BuiltinException. (format "array.slice: operand 2 must be number but got %s", (java->rego start)))))
-  (when-not (int? stop)
-    (throw (BuiltinException. (format "array.slice: operand 3 must be number but got %s", (java->rego stop)))))
+(defn builtin-slice
+  "Implementation of array.slice built-in"
+  {:builtin "array.slice" :args-types ["array", "number", "number"]}
+  [arr start stop]
+  (check-args (meta #'builtin-slice) arr start stop)
   (let [start (max start 0)
         stop (min stop (count arr))]
     (if (> start stop)
