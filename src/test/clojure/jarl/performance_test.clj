58261c1c643d716f87fd4476965f565c2a3695a6
Johan Fylling <johan.dev@fylling.se>
2022-07-01 01:11:59 +0200

    Adding TODO
    Signed-off-by: Johan Fylling <johan.dev@fylling.se>

--- src/test/clojure/jarl/performance_test.clj
+++ src/test/clojure/jarl/performance_test.clj
@@ -4,6 +4,7 @@
             [jarl.parser :as parser]
             [test.benchmark :refer [benchmark-jarl]]))
 
+;TODO: Compare against (2x?) last entry in jarl-perf.csv
 (deftest ^:performance perf-test
   (let [ir (parser/parse-file (io/resource "rego/perf/plan.json"))
         bench-result (benchmark-jarl ir)]
