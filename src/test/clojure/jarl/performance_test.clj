5346e0b4087f5cbf437f2fd379fe0bb8842800fb
Johan Fylling <johan.dev@fylling.se>
2022-07-02 21:15:51 +0200

    Disabling performance test
    to save on build time
    
    Signed-off-by: Johan Fylling <johan.dev@fylling.se>

--- src/test/clojure/jarl/performance_test.clj
+++ src/test/clojure/jarl/performance_test.clj
@@ -1,11 +1,12 @@
-(ns jarl.performance-test
-  (:require [clojure.test :refer [deftest]]
-            [clojure.java.io :as io]
-            [jarl.parser :as parser]
-            [test.benchmark :refer [benchmark-jarl]]))
+;(ns jarl.performance-test
+;  (:require [clojure.test :refer [deftest]]
+;            [clojure.java.io :as io]
+;            [jarl.parser :as parser]
+;            [test.benchmark :refer [benchmark-jarl]]))
 
 ;TODO: Compare against (2x?) last entry in jarl-perf.csv
-(deftest ^:performance perf-test
-  (let [ir (parser/parse-file (io/resource "rego/perf/plan.json"))
-        bench-result (benchmark-jarl ir)]
-    (println bench-result)))
+;FIXME: Disabled until we figure out what to do here, to save on test time on GH
+;(deftest ^:performance perf-test
+;  (let [ir (parser/parse-file (io/resource "rego/perf/plan.json"))
+;        bench-result (benchmark-jarl ir)]
+;    (println bench-result)))
