00f4c792bae8da3a6f6bf895783b7c58c156d3a5
Johan Fylling <johan.dev@fylling.se>
2022-10-23 16:21:01 -0400

    Making API Java 11 compliant
    Signed-off-by: Johan Fylling <johan.dev@fylling.se>

--- api/src/main/java/by/borge/jarl/Result.java
+++ api/src/main/java/by/borge/jarl/Result.java
@@ -1,10 +1,21 @@
 package by.borge.jarl;
 
 import java.util.Map;
+import java.util.Objects;
 
-public record Result(Object value) {
-    public static Result from(Map<String, ?> map) {
+public class Result {
+    private final Object value;
+
+    public Result(Object value) {
+        this.value = value;
+    }
+
+    public static Result fromResultMap(Map<String, ?> map) {
         return new Result(map.get("result"));
+    }
+
+    public Object getValue() {
+        return value;
     }
 
     @Override
@@ -12,5 +23,18 @@ public record Result(Object value) {
         return "Result{" +
                 "value=" + value +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof Result)) return false;
+        Result result = (Result) other;
+        return Objects.equals(value, result.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
--- api/src/main/java/by/borge/jarl/ResultSet.java
+++ api/src/main/java/by/borge/jarl/ResultSet.java
@@ -2,10 +2,19 @@ package by.borge.jarl;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
-public record ResultSet(List<Result> results) {
+import static java.util.Objects.requireNonNull;
+
+public class ResultSet {
+    private final List<Result> results;
+
     private static final ResultSet EMPTY = new ResultSet(List.of());
+
+    public ResultSet(List<Result> results) {
+        this.results = requireNonNull(results, "results list must not be null");
+    }
 
     public ResultSet(Result result) {
         this(List.of(result));
@@ -15,18 +24,26 @@ public record ResultSet(List<Result> results) {
         return EMPTY;
     }
 
-    public static ResultSet from(Map<String, ?> result) {
-        return from(List.of(result));
+    public static ResultSet fromResultsMap(Map<String, ?> result) {
+        return fromResultsList(List.of(result));
     }
 
-    public static ResultSet from(List<Map<String, ?>> list) {
+    public static ResultSet fromResultsList(List<Map<String, ?>> list) {
         if (list == null) {
             return empty();
         }
 
         return new ResultSet(list.stream()
-                .map(Result::from)
+                .map(Result::fromResultMap)
                 .collect(Collectors.toList()));
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public Result getFirst() {
+        return results.stream().findFirst().orElse(null);
     }
 
     @Override
@@ -34,5 +51,18 @@ public record ResultSet(List<Result> results) {
         return "ResultSet{" +
                 "results=" + results +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof ResultSet)) return false;
+        ResultSet resultSet = (ResultSet) other;
+        return results.equals(resultSet.results);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(results);
     }
 }
